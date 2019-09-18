cbdbdd6cdfb21c1eb4761630cfb8f8689981a89a
Shane Gallatin <shane.gallatin@verizon.net>
2019-09-18 16:07:19 -0500

    finished 5 problems

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr) {
+return arr.shift();
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr) {
+  return arr.pop();
+}
 
 
 
@@ -47,7 +51,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family) {
+  for (let i=0; i <= family.length-1; i++){
+    alert (family[i]); 
+  }
+}
 
 
 
@@ -62,7 +70,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper(letters){
+  for (let i = letters.length-1; i >= 0; i--){
+    alert(letters[i]); 
+  }
+}
 
 
 
@@ -76,8 +88,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Write a function named evenFinder that is given nums as it's only argument.
   Return an array that contains the even numbers from the nums array.
 */
-
-//Code Here
+let evenNums = []
+function evenFinder(nums){
+  for (let i = 0; i <= nums.length-1; i++){
+    if (nums[i] % 2 === 0) {
+     evenNums.push(nums[i]);
+    } 
+}
+return evenNums;
 
 
 
@@ -315,9 +333,4 @@ var user1 = {
 */
 
 //Code Here
-
-
-
-/*
-  The activity we just did is very much how data works in 'the real world'.
-*/
+}
